412ee406ccce92f5d20c6870920036d41e873dda
Pavitra Golchha <hello@pavi2410.me>
2024-09-24 21:51:49 +0530

    fix name checker page title again

--- pages/tools/name-checker.vue
+++ pages/tools/name-checker.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
+definePageMeta({
+  layout: false,
+})
+
+useHead({
+  title: 'Name Checker',
+  link: [
+    {
+      rel: 'icon',
+      href: `data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">™️</text></svg>`
+    }
+  ]
+})
+
+import { ref } from 'vue';
 
 interface SearchResult {
   name: string;
