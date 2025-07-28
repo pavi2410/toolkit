024eeb89198a5a0aca124e6eb5243c1c39fe6672
Pavitra Golchha <hello@pavi2410.me>
2025-07-28 07:53:59 +0530

    fix build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2025-07-01',
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/tailwindcss"]
+  modules: ["@nuxt/ui", "@vueuse/nuxt"],
+  css: ['~/assets/css/main.css'],
 })
