d2630c47b8d37ca602d782ab660dfd2fe36631a5
Pavitra Golchha <hello@pavi2410.com>
2026-04-14 19:29:46 +0530

    clean state

--- nuxt.config.ts
+++
@@ -1,7 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-07-01',
-  devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@vueuse/nuxt"],
-  css: ['~/assets/css/main.css'],
-})
